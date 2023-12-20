2acfeb6e7bb1da624d4c3d6c8aa44cd63f3529ad
maksim <maks.volontirov@gmail.com>
2023-12-21 02:42:12 +0300

    Task 2: Add QuestionsCMSComponent, data and controller

---
+++ custom/questions/src/com/es/questions/controllers/QuestionsCMSComponentController.java
@@ -0,0 +1,11 @@
+package com.es.questions.controllers;
+
+import de.hybris.platform.addonsupport.controllers.cms.AbstractCMSAddOnComponentController;
+import org.springframework.ui.Model;
+
+public class QuestionsCMSComponentController extends AbstractCMSAddOnComponentController<QuestionsCMSComponentModel> {
+    @Override
+    protected void fillModel(javax.servlet.http.HttpServletRequest request, Model model, QuestionsCMSComponentModel component) {
+
+    }
+}
