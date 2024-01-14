6b4939735141e375fe297cfa0d79054678dffc9f
maksim <maks.volontirov@gmail.com>
2024-01-15 00:22:25 +0300

    Task 4: Fix cms component

--- custom/questions/acceleratoraddon/web/src/com/es/questions/controllers/cms/QuestionsCMSComponentController.java
+++ custom/questions/acceleratoraddon/web/src/com/es/questions/controllers/cms/QuestionsCMSComponentController.java
@@ -1,34 +1,37 @@
-package com.es.questions.controllers;
+package com.es.questions.controllers.cms;
 
 import com.es.questions.facades.ProductFacade;
-import com.es.questions.model.QuestionCMSComponentModel;
+import com.es.questions.model.QuestionModel;
+import com.es.questions.model.QuestionsCMSComponentModel;
 import de.hybris.platform.acceleratorservices.data.RequestContextData;
 import de.hybris.platform.addonsupport.controllers.cms.AbstractCMSAddOnComponentController;
 import de.hybris.platform.core.model.product.ProductModel;
+import de.hybris.platform.product.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 import java.util.Objects;
 
-@Controller
-@RequestMapping("/views/QuestionCMSComponentController")
-public class QuestionsCMSComponentController extends AbstractCMSAddOnComponentController<QuestionCMSComponentModel> {
+@Controller("QuestionsCMSComponentController")
+@RequestMapping("/view/QuestionsCMSComponentController")
+public class QuestionsCMSComponentController extends AbstractCMSAddOnComponentController<QuestionsCMSComponentModel> {
     private static final String QUESTIONS = "questions";
 
     @Autowired
     private ProductFacade productFacade;
 
     @Override
-    protected void fillModel(HttpServletRequest request, Model model, QuestionCMSComponentModel component) {
+    protected void fillModel(HttpServletRequest request, Model model, QuestionsCMSComponentModel component) {
         RequestContextData data = getRequestContextData(request);
 
-        if (!Objects.nonNull(data)) {
+        if (Objects.nonNull(data)) {
             ProductModel product = data.getProduct();
 
-            if(!Objects.nonNull(product)) {
+            if(Objects.nonNull(product)) {
                 String code = product.getCode();
 
                 productFacade.getProduct(code).ifPresent(prod -> model.addAttribute(QUESTIONS, prod.getQuestions()));
--- custom/questions/acceleratoraddon/web/webroot/WEB-INF/views/responsive/cms/questionscmscomponent.jsp
+++ custom/questions/acceleratoraddon/web/webroot/WEB-INF/views/responsive/cms/questionscmscomponent.jsp
@@ -1,15 +1,16 @@
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@ taglib prefix="spring" uri="http://www.springframework.org/tags" %>
 <div>
+    <h1>Questions</h1>
     <c:forEach var="question" items="${questions}">
         <div>
-            <spring:message code="text.customer"/>${question.questionCustomerName}
-            <spring:message code="text.question"/>${question.question}
+            <h4><spring:message code="text.customer"/></h4>${question.questionCustomerName}
+            <h4><spring:message code="text.question"/></h4>${question.question}
         </div>
         <c:if test="${not empty question.answer}">
             <div>
-                <spring:message code="text.customer"/>${question.answerCustomerName}
-                <spring:message code="text.answer"/>${question.answer}
+                <h4><spring:message code="text.customer"/></h4>${question.answerCustomerName}
+                <h4><spring:message code="text.answer"/></h4>${question.answer}
             </div>
         </c:if>
     </c:forEach>
